76ad0412f555e507346b21352e0950179cb1dba7
Karan Misra <kidoman@gmail.com>
2014-09-13 04:49:01 +0530

    try and load pike.jpg

--- web.go
+++ web.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/png"
 	"net/http"
+	"os"
 )
 
+var pike image.Image
+
 func init() {
+	f, err := os.Open("pike.jpg")
+	if err != nil {
+		panic(err)
+	}
+	pike, _, err = image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/love.png", love)
 	http.HandleFunc("/", yum)
 }
 
 func yum(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Yum yum yum!")
+}
+
+func love(w http.ResponseWriter, r *http.Request) {
+	png.Encode(w, pike)
 }
